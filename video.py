abd5323cb025fbe4aa06d7fed29c4c645e3df5a3
samikshapandit <114738914+samikshapandit@users.noreply.github.com>
2023-07-25 20:53:35 +0530

    Add files via upload
    In this file, the code is available for capturing video using camera of your PC.

---
+++ video.py
@@ -0,0 +1,16 @@
+import cv2
+import time
+
+cam = cv2.VideoCapture(0)
+
+time.sleep(1)
+while True:
+    _,img = cam.read()
+    cv2.imshow("cameraFeed",img)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+    
+cam.release()
+cv2.closeAllWindows()
